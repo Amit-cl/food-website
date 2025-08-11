2a9234b7b96b3f714f1f7199dc3e486545bee770
amit <amitkumar2182004@gmail.com>
2025-08-12 00:05:09 +0530

    this is my first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { LOGO_URL } from './Constant';
+// filepath: c:\Users\Amit\OneDrive\Desktop\foodweb\foodweb\src\components\Header.jsx
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img src={LOGO_URL} alt="App Logo" className="logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
