70b8184003317d94f5bee508829d0f0d4871d9bb
amit <amitkumar2182004@gmail.com>
2025-08-14 23:00:36 +0530

    D \basic dynamic routing started

---
+++ src/components/RestrorantMenu.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const RestrorantMenu = () => {
+  return (
+    <div>
+      <h1>Name of Restruant</h1>
+      <ul>
+        <li>BIryani</li>
+         <li>burger</li>
+          <li>Diet coke</li>
+      </ul>
+      Biryani
+      
+      </div>
+    
+  )
+}
+
+export default RestrorantMenu
